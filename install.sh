bede335ec1a93dd4743faaf4ae65df7dd73e55d3
Dmitry Petrov <dpetroff@gmail.com>
2018-02-03 22:54:08 +0100

    tig

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Installing common packages"
-sudo apt-get install vim git curl jq tmux
+sudo apt-get install vim git curl jq tmux tig
 # clipboard doesn't work? Try installing vim-gtk3 instead
 
 echo "Setting up git config"
