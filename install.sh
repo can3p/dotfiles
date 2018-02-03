2244cf0abffadc9db03c9876d2c95bf77a3ea338
Dmitry Petrov <dpetroff@gmail.com>
2018-02-03 19:13:08 +0100

    gr alias, historical note about vim clipboard hell and more reliable way to get root folder of the script

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=`dirname $0`
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Installing common packages"
 sudo apt-get install vim git curl jq tmux
+# clipboard doesn't work? Try installing vim-gtk3 instead
 
 echo "Setting up git config"
 
